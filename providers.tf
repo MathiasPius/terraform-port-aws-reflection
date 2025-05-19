96a286c48ee64a6527ea5010bae6a953a1a9ed9f
Mathias Pius <contact@pius.io>
2025-05-19 21:20:01 +0200

    Add random provider

--- providers.tf
+++ providers.tf
@@ -4,9 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 5.0"
+    }
+
     port-labs = {
       source  = "port-labs/port-labs"
-      version = "~>2.7"
+      version = "~> 2.7"
     }
   }
 }
